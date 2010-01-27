12f96d8510417ae19eb8d072b7a0f6b4d1cc64be
cadorn <christoph@christophdorn.com>
2010-01-27 15:31:05 -0800

    refactored assert:deepEqual to UTIL.deepEqual(); new UTIL.deepDiff(); fixed UTIL.deepComplete()

---
+++ tests/util/diff.js
@@ -0,0 +1,62 @@
+var assert = require("assert");
+var util = require("util");
+
+exports["test deep diff"] = function() {
+    
+    var compare = {
+        "key1": "val1",
+        "key2": "val2",
+        "key3": {
+            "key4": "val4",
+            "key5": [
+                "val5",
+                "val6"
+            ],
+            "key7": "val7",
+            "key8": [
+                "val8"
+            ]
+        },
+        "key9": "val9",
+        "key10": {
+            "key11": "val11"
+        }
+    };
+    
+    var to = {
+        "key2": "val2",
+        "key3": {
+            "key4": "val4",
+            "key5": [
+                "val5"
+            ],
+            "key8": [
+                "val8"
+            ]
+        },
+        "key10": {
+            "key11": "val11"
+        }
+    }
+
+    var to2 = {
+        "key9": "val9"
+    }
+    
+    // return all properties in "compare" not present/different in/to "to" and "to2"
+    
+    assert.deepEqual(util.deepDiff(compare, to, to2), {
+        "key1": "val1",
+        "key3": {
+            "key5": [
+                "val5",
+                "val6"
+            ],
+            "key7": "val7"
+        }
+    });
+
+};
+
+if (require.main == module.id)
+    require("test").run(exports);
